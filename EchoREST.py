e23c77f00e450b1569899f5aa7decd25c28cba47
Atrayee <atrayee.ent.aec@gmail.com>
2016-12-26 12:31:51 +0530

    Various HTTP verbs using REST

---
+++ EchoREST.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PATCH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+if __name__ == '__main__':
+    app.run()
